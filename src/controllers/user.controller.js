815b403bda0242006dc165a23f7f87f1199d4180
aman-29-singh <singhaman41004@gmail.com>
2025-08-09 00:23:18 +0530

    pending loginUser api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,40 @@ import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+//ye method separately bana rahe hai because ye method bohat jaghah use hoga jab tokens banana hoga
+const generateAccessAndRefreshTokens = async(userId) => {
+  try{
+     const user = await User.findById(userId)
+     const accessToken = user.generateAccessToken()
+     const refreshToken = user.generateRefreshToken()
+     /**AccessToken toh server ye user client ko de deta hai  lekin RefreshToken ko server apne database mein bhi save 
+      * karke rakhta hai taki baar baar user se password na maangna pade user se toh abb RefreshToken ko database mein 
+      * kaise stor karein toh ye user jo hai ye object hai iske andar user client ki saari property hai toh jab user ki saari
+      * property aayi hai jaise user ka username aaya hai, password aaya hai,watchHistory aaya hai ussi tarah se
+      * refreshToken bhi aaya hai hai tto abb object k andar kaise value add karenge  so
+      */
+     user.refreshToken = refreshToken //toh user k andar humne refreshToken add kara diya but user ko save nhi karaya hai
+     //toh user ko save karayenge
+      await user.save({validateBeforeSave: false})//kyunki mongodb se banakar aaya hai toh humare pass save method hota hai
+     //validateBefore:false means validatte karne ki jarurat nhi hai yahaan refreshToken database mein store hogya
+    
+
+     /**so user client ka refreshToken and AccessToken dono abhi server k pass hai aur refreshToken ko database
+      * mein jaakaar store karwa chuka hoon lekin refreshToken ka reference abhi server mein hai huamre pass
+      * i.e above mein rakha hai reference i.e const refreshToken = user.generateRefreshToken()
+      * toh abb server kya karega ki accessToken and RefreshToken ko return kar dega yahan se
+      * toh abb ye method generateAccessAndRefreshToken(userId) ko jab bhi hum run karenge userId pass
+      * karke toh ye method apne aap hii user ko find kar lega userId k basis par aur uske baad AccessToken aur
+      * RefreshToken bhi generate kar lega aur uske baad ye user k refreshToken ko database mein save bhi kara diya
+      * aur fir last mein accessToken and refreshTToken ko return kar dega 
+      */
+     return {accessToken, refreshToken}
+    } catch(error){
+    throw new ApiError(500, "Something went wrong while generating refresh And Access token")
+  }
+}
+
 const registerUser = asyncHandler( async (req, res)=> {
     /**challenge itna hai ki humein user ko register karna hai but ye dikhne mein itna sa challenge hai but
      * itna nhi hai because ye challenge k steps bohat hai yahi chiz hai jo humari logic building exercise mein help
@@ -111,7 +145,8 @@ const registerUser = asyncHandler( async (req, res)=> {
   //so agar existedUser hai toh muje aage proceed hii nhi karna hai muje user ko sidha wahin k wahin error throw karna hai
   if(existedUser){
     throw new ApiError(409, "User with email or username already exists")
-  } 
+  }
+  console.log(req.files); 
 
   //STEPS4-- NOW Humein check karna hai images aur avatar wale images check karni padegi
   /*so abhi tak humne dekha hai ki server mein humare pass req.body k andar saara ka saara data aata hai
@@ -126,12 +161,18 @@ const registerUser = asyncHandler( async (req, res)=> {
   //console.log(avatarLocalPath) aise check bhi kar sakte hai 
   // avatarLocalPath isliye kyunki ye abhi humare server par hai ye abhi tak cloudinary par nhi gya hai
 
-  //same with coverImage
-  const coverImageLocalPath = req.files?.coverImage[0]?.path; //ie coverImage[0] ki first property se humein optionally ho sakta hai mil jaye ek path
-  console.log(avatarLocalPath, "ye error hai")
+  //same with coverImage advance tarika coverImageLocalPath  check karne ka i.e advance if else use karne
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path; //ie coverImage[0] ki first property se humein optionally ho sakta hai mil jaye ek path
+  
+  let coverImageLocalPath;
+  //so classic tarika check karne ka ki coverImageLocalPath
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverImageLocalPath = reqfiles.coverImage[0].path //isse coverImage="" aayega agar empty hoga toh but advance if else se error aata
+  }
+  //console.log(avatarLocalPath, "ye error hai")
   //toh humare pass server mein 2 LocalPath aagaye abb ye LocalPath honge ya nhi hone iski koi gurantee nhi hai ho bhi sakte hai nhi bhi ho sakte
   //but ek path to chaiye hii chaiye ki humare pass mein avatar wali image toh honi hii chaiye toh check karenge
-  if(!avatarLocalPath) {
+  if(!avatarLocalPath) { //ie avatar agar server k localPath par nhi mila toh throw the error
     throw new ApiError(400, "Avatar file is required")
   }
 
@@ -142,8 +183,8 @@ const registerUser = asyncHandler( async (req, res)=> {
 //iss cloudinary.js file mein ek uploadOnCloudinary method hai  jiska use karke hum file ko cloudinary par upload karenge
 //so uploadOnCloudinary(avatarLocalPath) pass kar dunga yeh avatar ko cloudinary k upar upload kar dega aur humein response mil jayega
 //ye uploadOnCloudinary() ye method time lagayega file ko upload karne k liye isliye we will use await
- const avatar = await uploadOnCloudinary(avatarLocalPath)
- const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+ const avatar = await uploadOnCloudinary(avatarLocalPath)//avatar agar avatarLocalPath mein mil gya toh cloudinary par upload kardo
+ const coverImage = await uploadOnCloudinary(coverImageLocalPath)//agar coverImageLocalPath nhi mila toh clodinary humein error nhi de rha bas ""empty string rturn kar deta hai
 
  //so ye avatar cloudinary par chala gya abb kyunki avatar required field hai agar nhi hoga toh database phatega
  //toh avatar ko ek baar phir check karenge
@@ -181,7 +222,7 @@ const createdUser = await User.findById(user._id).select( //THIS IS STEP 7 i.e r
 ) 
  
  //STEP 8 --abb check karenge user creation
- if(!createdUser) {
+ if(!createdUser) {//abb agar user nhi create hua toh error dedo
   throw new ApiError(500, "Something went wrong while registering a user")
  }
 
@@ -197,4 +238,95 @@ const createdUser = await User.findById(user._id).select( //THIS IS STEP 7 i.e r
 
   })
 
-export {registerUser}
+
+
+
+
+ 
+  /*Note concecpt eh hai inn dono tokens k bich mein i.e refresh token and access token k bich mein
+ki jab tak aapke pass i.e user k pass Access Token hai tab tak aap koi aap  bhi fetaure jaha par 
+aapki i.e client ki authentication ki requirement hai waha par aap access kar saktee ho uss resource ko
+suppose karo har kisi ko file Upload toh nhi karne diya jaa sakta server par toh agar aap i.e user autthenticated
+ho login ho toh toh aap kar loge lekin agar maan lijiye ki aapka i.e user ka login session maine 15 minutte k 
+andar hii maine i.e server ne expire kar diya for security reasons ki wajah se toh phir user ko 15 minute k baad
+password insert karna padega  aur user ko phirse login karna padega yahin par exact point par humaara aata 
+hai refresh Token ab  ye Refresh Token kya hai hum database mein bhi store rakhte hai aur User ko bhi refresh Token dette hai
+toh server ye User ko validate toh access Token se hii karta hai lekin server ye User ko bol deta hai ki har baar User
+ko password daalne ki ki jarurat nhi hai agar User k pass User ka Refresh Token hai toh ek endPoint hit kardo
+wahan se agar User k pass jo Refresh TToken hai aur server k pass jo database mein sttore kiya hua Refresh Token hai
+ye dono Refresh Token agar same hogye n toh server and User ye dono mandwali kar lenge i.e aapas mein baat kar lenge
+aur server ye User ko ek new Access Token de dega so here Acess Token ye short lived hai and Refresh Token ye long lived hai      */
+
+/**so abb hum ye dono methods ko use karenge i.e Access Token and Refresh Token ko aur isko use karke hum login
+ * User banayenge
+ */
+const loginUser = asyncHandler(async (req, res) => {
+    //steps ki hum /login ka controller kaise banayenge
+    //Step1-> req.body se user ka data leke aao
+    //STEP-2-> check Username,email etc wagera correct hai ya nhi i.e validatae hai ya nhi i.e kahin user ne emptty value toh nhi send kar diya
+    /*YAHAN par server k upar hai ki server ye username based access dena chahte ho user ko ya email based access dena chahte ho
+    i.e username or email dono mein se ek pe aap user ko login karwa sakte ho but hum code aisa likhenge ki ek hii 
+    code dono par kaam karega matlab agaar koi email based bana raaha hai toh uspe bhi kaam kar jaye aur agar koi username 
+    based login bana rha hai toh uspe bhi kaam kar jaye */
+    /*STEP-3-> find the user ki User humare pass database mein hai ya nhi so agar user nhi hai database mein toh obvious si baat hai
+    mein i.e server bol dega user ki request ko ki user hai hii nhi yahan par agar user mil jata hai database mein toh kya karna 
+    padega toh login hai toh user ka password check karwao toh agar password check nhi hua ttoh server user ko bo dega
+    ki password aapka wrong hai so */
+    //STEP-4-> PASSWORD CHECK
+    /*STEP-5-> Access Token  and Refreh Token so agar User ka paswword check means correct hogya toh server ko 
+    Access and Refresh Token dono generate karne padenge aur hii dono User ko bhejenge
+    so jab humne models design kiye the upar above mein toh humne iss tarah  se kar liya tha ki ye jo
+    AccessToken hai ye toh generate ho jata hai easily aur humein User i.e client ko mil jata hai jwtt.sign aur RefreshToken ye bhi 
+    client k pass Generate ho jata hai to 
+    so User k pass ACCESS tOKEN BHI gENERATE HOGYA AND Refresh Token bhi generate hogya so   */
+    /*STEP-6 -> iske baad inn Tokens ko bhej doo user ko Cookies mein send kardo toh hum secure Cookies 
+    bhejte hai aur User ko Response bhej do ek ki successfully login hogya hai so 
+     HERE yahan par STEP-6 is all about send Cookies */
+
+    //STEP-1
+    const { email, username, password } = req.body
+
+    if (!username || !email) { //agar user k pass username or email dono nhi hai toh error send karo
+        throw new ApiError(400, "username or password is required")
+        //agar sirf email se hii login karwana tha toh hum if(!email) karke check karte
+    }
+
+    /*so agar aap user already Registered ho toh user login kar sakta hai toh matlab ya toh username ka koi hoga mere pass database mein
+    ya toh email ka hoga toh dono ko kaise find kar sakte   */
+    const user = await User.findOne({
+        //toh server find karna chahta hai ki ya toh email find kardo ya phir username find kardo toh hum $ sin use kar sakte hai
+        //$or,$and etc ye mongodb k operators hai
+        $or: [{ username }, { email }]//Toh abB ye $or operator find karegaa ek value ko ya toh woh username k basis par mil jaye ya woh email k base par mil jaye
+
+    })
+    //toh agar ye $or laga k inn dono k basis par i.e username or email $or laga kar bhi User nhi m,ila means woh User kabhi Registered tha hii nhi
+    //STEP-3 CHECK USERS IN DATABASE
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    //Steps-4 agar user mil gya toh password check karna padega so password kaise check karenge
+    //so above humare pass method ban sakte hai isPasswordCorrect() kaise bcrypt humein help kar sakta hai password check karne k liye
+    //but iss bcrypt ko humein dena padega ek password dena padega aur ek current User k andar se bhi password dena padega
+    //i.e bcrypt.compare(password, this.password) idhar ye password hai jo hum denge aur this.password hai current user k andar ka password
+    //so humein jo issPassword = async function(password) jo ye wala password hai ye user wala password hai jo user client side pass karega
+    //aur jo this.password hai ye saved user ka password hai jo database mein save user hai uss database ka andar k user ka password hai
+     const isPasswordValid = await user.isPasswordCorrect(password)//ye user humara user hai ye mongoose ka User nhi hai toh iss user k pass humara define kiya hua methods hai
+     //so so ye wala humara user i.e client ka hai jo humne database se wapas liya hai toh iske pass humara wala methods hoga
+     // ispasswordCorrect(password) ye humne req.body se nikala hai password jo user ne client side se bheja hai
+     //abb agar isPasswordCorrect thik hai toh obvious si baat hai aage chaliye aur agar password thik nhi toh error dena padega
+     if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials ")
+    }//toh abb user ka password bhi check hogya hai
+
+    //step-5 toh agar user ka password bhi thik hai toh Access Token and Refresh Token banao
+    //abb ye Access and Refresh Token wala kaam hum kahin baar karenge in fact ye kaam itna common hai ki ho saktta hai kahin baar iska use aayega
+    //so kahin baar Ye Access Token and Refresh token ka use aayega isliye method above banayenge
+    //SO METHOD ABOVE UPAR BANAYAENGE i.e const generateAccessAndRefreshTokens= async(userId) toh iss user humare user k through userId le sakte hai
+    
+
+
+
+    })
+
+export {registerUser,loginUser}
